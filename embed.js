6a81c43ca3bd5a0749c7269f980f96f34fe0d82e
Shav <shavenchew@yahoo.com>
2016-11-30 14:21:20 -0800

    Starter Data

--- embed.js
+++ embed.js
@@ -4,19 +4,51 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/blog_demo');
 
 //Models
-//User Model
-var userSchema = new mongoose.Schema({
-  email : String,
-  name: String
-});
-
-var Users = mongoose.model('Users', userSchema);
-
-
 //Post Model
 var postSchema = new mongoose.Schema({
   title: String,
   content: String
 });
-
 var Posts = mongoose.model('Posts', postSchema);
+
+
+//User Model
+var userSchema = new mongoose.Schema({
+  email : String,
+  name: String,
+  posts: [postSchema]
+  // posts: [
+  //   {title: 'dfadfas', content: 'sdfafafadf'},
+  //   {title: 'dfadfas', content: 'sdfafafadf'}
+  // ]
+});
+var User = mongoose.model('User', userSchema);
+
+
+// var newPost = new Posts({
+//   title: 'This is a test',
+//   content: 'I think I love Pepsi?'
+// });
+//
+// newPost.save(function(err, result){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log('Successfully Added: ');
+//     console.log(result);
+//   }
+// });
+
+// var newUser = new User({
+//   email : 'dodo@brown.edu',
+//   name : 'Dodo'
+// });
+//
+// newUser.save(function(err, result){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log('Successfully Added: ');
+//     console.log(result);
+//   }
+// });
