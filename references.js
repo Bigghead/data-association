4a8dbf79e25c963568400ca67e06d95565eec600
Shav <shavenchew@yahoo.com>
2016-11-30 15:51:15 -0800

    Using .populate to fill in the user's posts data

--- references.js
+++ references.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 //connect mongoose
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/blog_demo_2');
 
 //Models
@@ -37,29 +38,41 @@ var User = mongoose.model('User', userSchema);
 //   }
 // });
 
-Posts.create({
-  title: 'My Wealth',
-  content: 'How much money they pay me to troll Hobits'
-}, function(err, result){
-  if(err){
-    console.log(err);
-  } else {
-    //find a user that you want to add this post onto
-    User.findOne({name: 'Sauron'}, function(err, foundUser){
-      if(err){
-        console.log('Broken');
-      } else {
-        foundUser.posts.push(result);
+// Posts.create({
+//   title: 'Tips For A Successful Cookout',
+//   content: 'How to choose the best meat for your burgers pt. 1'
+// }, function(err, result){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     //find a user that you want to add this post onto
+//     User.findOne({name: 'Sauron'}, function(err, foundUser){
+//       if(err){
+//         console.log('Broken');
+//       } else {
+//         foundUser.posts.push(result);
+//
+//         //dont forget to save the new data
+//         foundUser.save(function(err, data){
+//           if(err){
+//             console.log(err);
+//           } else {
+//             console.log('Successful Add: ' + data);
+//           }
+//         });
+//       }
+//     });
+//   }
+//   });
 
-        //dont forget to save the new data
-        foundUser.save(function(err, data){
-          if(err){
-            console.log(err);
-          } else {
-            console.log('Successful Add: ' + data);
-          }
-        });
-      }
-    });
-  }
+
+//find User
+//find all posts by this user
+User.findOne({name: 'Sauron'}).populate('posts')
+.exec(function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(foundUser);
+    }
   });
